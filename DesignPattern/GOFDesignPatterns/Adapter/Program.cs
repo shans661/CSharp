95c14d62610eeba52ea93c4cf6d148ad04b2bb1e
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-07 08:10:37 +0530

    Adapter design pattern

---
+++ DesignPattern/GOFDesignPatterns/Adapter/Program.cs
@@ -0,0 +1,25 @@
+﻿using Adapter.Features;
+using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------");
+            Compund water = new RichCompound("Water");
+            water.Display();
+
+            Console.WriteLine("-----------------------------------");
+            Compund benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Console.WriteLine("-----------------------------------");
+            Compund ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+
+            Console.ReadKey();
+        }
+    }
+}

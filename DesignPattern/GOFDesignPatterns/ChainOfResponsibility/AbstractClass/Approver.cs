59087fd6c2a25aad8854391dc72aa54cc3e8e104
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-10 07:57:28 +0530

    Chain of responsibility design pattern implementation

---
+++ DesignPattern/GOFDesignPatterns/ChainOfResponsibility/AbstractClass/Approver.cs
@@ -0,0 +1,27 @@
+﻿using ChainOfResponsibility.Features;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility.AbstractClass
+{
+    /// <summary>
+    /// Abract class - Approver
+    /// </summary>
+    abstract class Approver
+    {
+        public Approver Successor { get; set; }
+
+        /// <summary>
+        /// Sets to successor to handle the request
+        /// </summary>
+        /// <param name="approver"></param>
+        public abstract void SetSuccessor(Approver approver);
+
+        /// <summary>
+        /// Method to process the request and send to superior if 
+        /// </summary>
+        /// <param name="purchase"></param>
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+}
---
+++ DesignPattern/GOFDesignPatterns/ChainOfResponsibility/Features/Approvers/VicePresident.cs
@@ -0,0 +1,30 @@
+﻿using ChainOfResponsibility.AbstractClass;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility.Features.Approvers
+{
+    /// <summary>
+    /// Create class - Approver - Vice president
+    /// </summary>
+    class VicePresident : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Price < 20000)
+            {
+                Console.WriteLine($"{purchase.ProductName} purchase request handled by {this.GetType().Name}");
+            }
+            else if (Successor != null)
+            {
+                Successor.ProcessRequest(purchase);
+            }
+        }
+
+        public override void SetSuccessor(Approver approver)
+        {
+            Successor = approver;
+        }
+    }
+}

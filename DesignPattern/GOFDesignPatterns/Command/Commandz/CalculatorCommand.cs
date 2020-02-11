da23c051c078cd269abfbf747cc41adccb111d81
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-11 22:30:14 +0530

    Command design pattern implementation

---
+++ DesignPattern/GOFDesignPatterns/Command/Commandz/CalculatorCommand.cs
@@ -0,0 +1,47 @@
+﻿using Command.Receiver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command.Commandz
+{
+    /// <summary>
+    /// Concrete command class
+    /// </summary>
+    public class CalculatorCommand : Commands
+    {
+        Calculator calculator = new Calculator();
+
+        public string Operator { get; set; }
+        public int operand { get; set; }
+
+        public int Result { get; set; }
+        public void Execute(string Op, int x)
+        {
+            Operator = Op;
+            operand = x;
+            Result = calculator.Operation(Op, x);
+        }
+
+        /// <summary>
+        /// Keep the previous in property
+        /// </summary>
+        public void UnExecute()
+        {
+            Result = calculator.Operation(Undo(), operand, Result);
+        }
+
+        private string Undo()
+        {
+            switch(Operator)
+            {
+                case "+": return "-";
+                case "-": return "+";
+                case "*": return "/";
+                case "/": return "*";
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ DesignPattern/GOFDesignPatterns/Command/Receiver/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command.Receiver
+{
+    /// <summary>
+    /// Invoker class
+    /// </summary>
+    class Calculator
+    {
+        public int Operation(string Op, int x, int result=0)
+        {
+            switch(Op)
+            {
+                case "+":
+                    result = x + result;
+                    break;
+                case "-":
+                    result = result - x;
+                    break;
+                case "*":
+                    result = x * result;
+                    break;
+                case "/":
+                    result = result / x;
+                    break;
+            }
+            Console.WriteLine($"Result is {result}");
+            return result;
+            
+        }
+    }
+}

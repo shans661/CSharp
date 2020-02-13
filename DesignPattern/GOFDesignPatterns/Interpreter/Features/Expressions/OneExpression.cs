66f05ab727ad88d25dda3f5de62d297ac3ef57f0
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-13 07:49:20 +0530

    Interpreter design pattern

---
+++ DesignPattern/GOFDesignPatterns/Interpreter/Features/Expressions/OneExpression.cs
@@ -0,0 +1,35 @@
+﻿using Interpreter.AbstractClas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpreter.Features.Expressions
+{
+    class OneExpression : Expression
+    {
+        public override string Five()
+        {
+            return "V";
+        }
+
+        public override string Four()
+        {
+            return "IV";
+        }
+
+        public override int Multiplier()
+        {
+            return 1;
+        }
+
+        public override string Nine()
+        {
+            return "IX";
+        }
+
+        public override string One()
+        {
+            return "I";
+        }
+    }
+}
---
+++ DesignPattern/GOFDesignPatterns/Interpreter/Features/Expressions/TausendExpression.cs
@@ -0,0 +1,38 @@
+﻿using Interpreter.AbstractClas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpreter.Features.Expressions
+{
+    /// <summary>
+    /// Concrete terminal expression
+    /// </summary>
+    class TausendExpression : Expression
+    {
+        public override string Five()
+        {
+            return "";
+        }
+
+        public override string Four()
+        {
+            return "";
+        }
+
+        public override int Multiplier()
+        {
+            return 1000;
+        }
+
+        public override string Nine()
+        {
+            return "";
+        }
+
+        public override string One()
+        {
+            return "M";
+        }
+    }
+}

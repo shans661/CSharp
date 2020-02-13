a2a0f074fc996b32b990309ec688849446f3a5a3
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-13 22:21:14 +0530

    Iterator design pattern implementation

---
+++ DesignPattern/GOFDesignPatterns/Interator/Interfaces/IIterator.cs
@@ -0,0 +1,19 @@
+﻿using Interator.Features;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interator.Interfaces
+{
+    /// <summary>
+    /// Iterface for the iterator
+    /// </summary>
+    interface IIterator
+    {
+        Item CurrentItem();
+        Item NextItem();
+        Item FirstItem();
+        Item LastItem();
+        bool IsDone();
+    }
+}
---
+++ DesignPattern/GOFDesignPatterns/Interator/Program.cs
@@ -0,0 +1,32 @@
+﻿using Interator.Features;
+using System;
+
+namespace Interator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------");
+            Collection collection = new Collection();
+            collection.Items.Add(new Item("Item-A"));
+            collection.Items.Add(new Item("Item-B"));
+            collection.Items.Add(new Item("Item-C"));
+            collection.Items.Add(new Item("Item-D"));
+            collection.Items.Add(new Item("Item-E"));
+            collection.Items.Add(new Item("Item-F"));
+
+            Iterator iterator = collection.CreateIterator();
+            iterator.FirstItem();
+            iterator.NextItem();
+            iterator.CurrentItem();
+            iterator.IsDone();
+            iterator.NextItem();
+            iterator.LastItem();
+            iterator.CurrentItem();
+            iterator.IsDone();
+            Console.WriteLine("----------------------------");
+            Console.ReadKey();
+        }
+    }
+}

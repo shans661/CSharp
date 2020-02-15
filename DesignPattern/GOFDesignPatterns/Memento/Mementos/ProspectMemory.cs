16c68cb8034a97cc01adbc51cf109993a25dda9f
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-15 18:05:38 +0530

    Memento design pattern implementation

---
+++ DesignPattern/GOFDesignPatterns/Memento/Mementos/ProspectMemory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento.Mementos
+{
+    /// <summary>
+    /// Class which internal stores the state of sales
+    /// </summary>
+    class ProspectMemory
+    {
+        private Mement m_Memento;
+        public Mement Memento
+        {
+            get { return m_Memento; }
+            set { m_Memento = value; }
+        }
+    }
+}
---
+++ DesignPattern/GOFDesignPatterns/Memento/Sales/SalesProspect.cs
@@ -0,0 +1,39 @@
+﻿using Memento.Mementos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento.Sales
+{
+    /// <summary>
+    /// Sales prospect class
+    /// </summary>
+    class SalesProspect
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+        public string Budget { get; set; }
+
+        public Mement SaveState()
+        {
+            Console.WriteLine("Save state initiated");
+            return new Mement(Name, Phone, Budget);
+        }
+
+        public void RestoreState(Mement memento)
+        {
+            Name = memento.Name;
+            Phone = memento.Phone;
+            Budget = memento.Budget;
+            Console.WriteLine("Restore state is completed");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Phone: {Phone}");
+            Console.WriteLine($"Budget: {Budget}");
+        }
+    }
+}

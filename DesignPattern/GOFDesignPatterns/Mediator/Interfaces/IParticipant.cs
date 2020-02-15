3ee4228b12206754325cef90899c3f28fd59f54c
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-15 15:41:57 +0530

    Mediator design pattern

---
+++ DesignPattern/GOFDesignPatterns/Mediator/Interfaces/IParticipant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator.Interfaces
+{
+    interface IParticipant
+    {
+        void Notify(string from, string message);
+    }
+}
---
+++ DesignPattern/GOFDesignPatterns/Mediator/Program.cs
@@ -0,0 +1,33 @@
+﻿using Mediator.Features;
+using Mediator.Interfaces;
+using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------------");
+            ChartRoom chartRoom = new ChartRoom();
+            var shivaParticipant = new Beatle("Shiva");
+            shivaParticipant.ChatRoom = chartRoom;
+            var shankaraParticipant = new Beatle("Shankara");
+            shankaraParticipant.ChatRoom = chartRoom;
+            var swathiParticipant = new Beatle("Swathi");
+            swathiParticipant.ChatRoom = chartRoom;
+            var shruthiParticipant = new Beatle("Shruthi");
+            shruthiParticipant.ChatRoom = chartRoom;
+
+            chartRoom.Register(shivaParticipant.Name, shivaParticipant);
+            chartRoom.Register(shankaraParticipant.Name, shankaraParticipant);
+            chartRoom.Register(swathiParticipant.Name, swathiParticipant);
+            chartRoom.Register(shruthiParticipant.Name, shruthiParticipant);
+
+            shivaParticipant.ChatRoom.Send(shivaParticipant.Name, shankaraParticipant.Name, "Hello shan i'm shiv");
+            shruthiParticipant.ChatRoom.Send(shruthiParticipant.Name, shivaParticipant.Name, "Hey how r u?");
+            Console.WriteLine("------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}

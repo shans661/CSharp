3b18d1e47da5891435c04dcef971322b4205e668
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-16 17:08:32 +0530

    State design pattern implementation

---
+++ DesignPattern/GOFDesignPatterns/State/States/AbstractState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State.States
+{
+    abstract class AbstractState
+    {
+        public int LowerLimit { get; set; }
+        public int UpperLimit { get; set; }
+        public int InterstRate { get; set; }
+        public int Balance { get; set; }
+        public Account Account { get; set; }
+
+        public AbstractState(int amount, Account account)
+        {
+            Balance = amount;
+            Account = account;
+        }
+        public abstract void Deposit(int amount);
+        public abstract void WithDraw(int amount);
+        public abstract void PayInterest();
+    }
+}

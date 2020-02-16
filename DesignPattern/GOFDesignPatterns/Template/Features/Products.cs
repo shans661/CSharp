0a07594642ac84840fe06cec63628c33a78cacbe
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2020-02-16 21:30:37 +0530

    Template design pattern

---
+++ DesignPattern/GOFDesignPatterns/Template/Features/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Features
+{
+    /// <summary>
+    /// Implementation of the products
+    /// </summary>
+    class Products : DataAccessObject
+    {
+        public override void Process()
+        {
+            Console.WriteLine("Product is processed");
+        }
+
+        public override void Select()
+        {
+            Console.WriteLine("Product is selected for the process");
+        }
+    }
+}

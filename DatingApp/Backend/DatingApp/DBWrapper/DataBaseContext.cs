b5162d57baef2fa42a5d738419516918ad69e0f5
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2021-02-13 19:09:33 +0530

    Backend - Dependency injection changes

---
+++ DatingApp/Backend/DatingApp/DBWrapper/DataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using DBWrapper.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBWrapper
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}

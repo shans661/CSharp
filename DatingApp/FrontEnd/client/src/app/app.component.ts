bd2811f952a34ccb23829ffca5e25750a94f49ad
Shiva shankara <Shiva shankara@DESKTOP-CG7TM84>
2021-02-14 22:05:12 +0530

    Client project initialization

---
+++ DatingApp/FrontEnd/client/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  
+  title = 'The dating app';
+  users: any;
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:44347/api/user").subscribe(res =>
+     {this.users = res}, er => {console.log(er)});
+  }
+
+}

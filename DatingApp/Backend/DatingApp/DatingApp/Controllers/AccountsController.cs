3ac450db5a443fa431ab0e51cb5d9151d5f6addf
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2021-02-16 18:00:44 +0530

    Register APi implementation

---
+++ DatingApp/Backend/DatingApp/DatingApp/Controllers/AccountsController.cs
@@ -0,0 +1,55 @@
+﻿using DBWrapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DBWrapper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        internal readonly DataBaseContext m_Context;
+        public AccountsController(DataBaseContext context) 
+        {
+            m_Context = context;
+        }
+
+        [Route("api/resister")]
+        public async Task<ActionResult> Register(string userName, string password)
+        {
+            var isUserExists = await m_Context.User.AnyAsync(x => x.UserName == userName);
+
+            if(isUserExists)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var user = new User();
+                HMACSHA512 encryptor = new HMACSHA512();
+
+                user.UserName = userName;
+                user.PasswordHash = encryptor.ComputeHash(Encoding.UTF8.GetBytes(password));
+                user.PasswordSalt = encryptor.Key;
+
+                await m_Context.User.AddAsync(user);
+                var result = await m_Context.SaveChangesAsync();
+
+                if(result == 1)
+                {
+                    return Accepted();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+        }
+    }
+}
--- DatingApp/Backend/DatingApp/DatingApp/Controllers/UserController.cs
+++ DatingApp/Backend/DatingApp/DatingApp/Controllers/UserController.cs
@@ -1,20 +1,26 @@
-﻿using DBWrapper.Entities;
+﻿using DBWrapper;
+using DBWrapper.Entities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 
 namespace DatingApp.API.Controllers
 {
-
-    public class UserController : BaseController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
     {
 
         private readonly ILogger<UserController> _logger;
+        internal readonly DataBaseContext m_Context;
 
-        public UserController(ILogger<UserController> logger)
+        public UserController(ILogger<UserController> logger, DataBaseContext context)
         {
             _logger = logger;
+            m_Context = context;
         }
 
+        [Route("api/users")]
         public IEnumerable<User> Get()
         {
             return new List<User>()

c7c514dc7f32412ccc73d65fcd03fc3f5c4b8ddd
M, Shiva Shankara (CT RDA DS AA EM DG-PRO PA PQ ALY) <shiva.m@siemens.com>
2021-05-31 16:24:30 +0530

    Caching member details

--- DatingApp/FrontEnd/client/src/app/services/member.service.ts
+++ DatingApp/FrontEnd/client/src/app/services/member.service.ts
@@ -13,34 +13,33 @@ import { UserParams } from '../Models/userParams';
 export class MemberService {
 
   members: Member[] = [];
-  paginatedResult : PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
+  paginatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
   memberCache = new Map();
 
   constructor(private httpClient: HttpClient) { }
 
-  getMember(username){
-    let member = this.members.find(x => x.userName === username);
+  getMember(username) {
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((x: Member) => x.userName === username);
 
-    if(member)
-    {
+    if (member) {
       return of(member);
     }
-    return this.httpClient.get<Member>(environment.apiUrl + "username/"+username);
+    return this.httpClient.get<Member>(environment.apiUrl + "username/" + username);
   }
 
-  getMembers(userParams: UserParams){
+  getMembers(userParams: UserParams) {
 
     var response = this.memberCache.get(Object.values(userParams).join('-'));
 
-    if(response)
-    {
+    if (response) {
       return of(response);
     }
 
     let params = new HttpParams();
 
-    if(userParams)
-    {
+    if (userParams) {
       params = params.append('pageNumber', userParams.pageNumber.toString());
       params = params.append('pageSize', userParams.pageSize.toString());
       params = params.append('gender', userParams.gender.toString());
@@ -48,23 +47,21 @@ export class MemberService {
       params = params.append('maxAge', userParams.maxAge.toString());
       params = params.append('orderBy', userParams.orderBy);
     }
-    return this.httpClient.get<Member[]>(environment.apiUrl + "users", {observe : 'response', params}).pipe(
+    return this.httpClient.get<Member[]>(environment.apiUrl + "users", { observe: 'response', params }).pipe(
       map(response => {
         this.paginatedResult.result = response.body;
-        if(response.headers.get('Pagination') !== null)
-        {
+        if (response.headers.get('Pagination') !== null) {
           this.paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
         }
-        this.memberCache.set(Object.values(userParams).join('-'), {...this.paginatedResult});
+        this.memberCache.set(Object.values(userParams).join('-'), { ...this.paginatedResult });
         return this.paginatedResult;
       })
     );
   }
 
-  updateMember(member: Member){
-    return this.httpClient.put(environment.apiUrl+"update", member).pipe(
-      map(() =>
-      {
+  updateMember(member: Member) {
+    return this.httpClient.put(environment.apiUrl + "update", member).pipe(
+      map(() => {
         let index = this.members.indexOf(member);
         this.members[index] = member;
       })

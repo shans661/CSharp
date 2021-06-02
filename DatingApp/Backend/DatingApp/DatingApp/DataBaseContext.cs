468cd83b0d1542d4abd942184e8bd9201cef0c3f
Shiva <shans661@gmail.com>
2021-06-02 20:07:33 +0530

    UserLike entity added

--- DatingApp/Backend/DatingApp/DatingApp/DataBaseContext.cs
+++ DatingApp/Backend/DatingApp/DatingApp/DataBaseContext.cs
@@ -11,5 +11,26 @@ namespace DatingApp.API
 
         public DbSet<AppUser> User { get; set; }
         public DbSet<Photo> Photo { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+            .HasKey(x => new {x.SourceUserId, x.LikedUserId});
+
+            builder.Entity<UserLike>()
+            .HasOne(x => x.SourceUser)
+            .WithMany(x => x.LikedUsers)
+            .HasForeignKey(x => x.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+            .HasOne(x => x.LikedUser)
+            .WithMany(x => x.LikedByUsers)
+            .HasForeignKey(x => x.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
---
+++ DatingApp/Backend/DatingApp/DatingApp/Entities/UserLike.cs
@@ -0,0 +1,10 @@
+namespace DatingDatingApp.API.Entities
+{
+    public class UserLike
+    {
+        public AppUser SourceUser { get; set; }
+        public int SourceUserId { get; set; }
+        public AppUser LikedUser { get; set; }
+        public int LikedUserId { get; set; }
+    }
+}

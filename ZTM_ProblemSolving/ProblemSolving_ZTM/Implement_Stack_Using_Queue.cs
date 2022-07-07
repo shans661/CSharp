05becc1cf4afe0133c2d76e0f9aaa8309bcc6f0d
shiva shankara <shans661@gmail.com>
2022-07-07 11:41:47 +0530

    Implement Stack using single queue

---
+++ ZTM_ProblemSolving/ProblemSolving_ZTM/Implement_Stack_Using_Queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_ZTM
+{
+    public class MyStack
+    {
+        Queue<int> myQueue;
+        public MyStack()
+        {
+            myQueue = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            if(myQueue.Count == 0)
+            {
+                myQueue.Enqueue(x);
+            }
+            else
+            {
+                int existingCount = myQueue.Count;
+                myQueue.Enqueue(x);
+                for (int count = 0; count < existingCount; count++)
+                {
+                    myQueue.Enqueue(myQueue.Dequeue());
+                }
+            }
+        }
+
+        public int Pop()
+        {
+           return myQueue.Dequeue();
+        }
+
+        public int Top()
+        {
+            return myQueue.Peek();
+        }
+
+        public bool Empty()
+        {
+            return !myQueue.Any();
+        }
+    }
+
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * MyStack obj = new MyStack();
+     * obj.Push(x);
+     * int param_2 = obj.Pop();
+     * int param_3 = obj.Top();
+     * bool param_4 = obj.Empty();
+     */
+}
--- ZTM_ProblemSolving/ProblemSolving_ZTM/Program.cs
+++ ZTM_ProblemSolving/ProblemSolving_ZTM/Program.cs
@@ -4,8 +4,8 @@
 //string outputString = ReverseString.Reverse(testString);
 //Console.WriteLine(outputString);
 
-Solution solution = new Solution();
-solution.IsValid("()[]{}");
+//Solution solution = new Solution();
+//solution.IsValid("()[]{}");
 
 //LinkedList linkedList = new LinkedList();
 ////linkedList.StackOverFlow();
@@ -14,3 +14,12 @@ solution.IsValid("()[]{}");
 //linkedList.Append("shiv");
 //linkedList.Append("shan");
 //linkedList.ShowAll();
+
+// Implement stack using single queue
+MyStack myStack = new MyStack();
+myStack.Push(1);
+myStack.Push(2);
+myStack.Push(3);
+Console.WriteLine($"{myStack.Pop()}"); 
+Console.WriteLine($"{myStack.Top()}"); 
+Console.WriteLine($"{myStack.Empty()}"); 
